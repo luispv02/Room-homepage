4149afe6d04b07bf0af04acbade8a6f52ee7a2d9
Luis Fernando <luispv.1002@gmail.com>
2021-03-24 18:06:15 -0600

    Proyecto Mobile Finalizado

--- js/app.js
+++ js/app.js
@@ -1,40 +1,95 @@
-let abrir = document.querySelector('.abrir');
+let abrir = document.querySelector('.abrir')
 let cerrar = document.querySelector('.cerrar');
-let body = document.querySelector('body')
 
-abrir.addEventListener('click', abrirMenu);
-cerrar.addEventListener('click', cerrarMenu);
 
-function abrirMenu(){
+let imagenes = ['images/mobile-image-hero-1.jpg', 'images/mobile-image-hero-2.jpg' , 'images/mobile-image-hero-3.jpg']
+let cont = 0
+console.log(cont)
+function carrousel(contenedor){
+    contenedor.addEventListener('click', (e) => {
+        let atras = contenedor.querySelector('.atras');
+        let adelante = contenedor.querySelector('.adelante');
+        let img = contenedor.querySelector('img');
+        let uno = document.querySelector('.one')
+        let dos = document.querySelector('.two')
+        let tres = document.querySelector('.three')
+       
+        
+        
+        if(e.target == atras){
+            if(cont > 0){
+                img.src = imagenes[cont - 1];
+                cont--;
+                console.log(cont)
+                if(cont == 1){
+                    tres.style.display = 'none'
+                    dos.style.display = 'block'
+                }else if(cont == 0){
+                    dos.style.display = 'none'
+                    uno.style.display = 'block'
+                }
+            }else{
+                img.src = imagenes[imagenes.length - 1];
+                cont = imagenes.length - 1;
+                console.log(cont)
+                if(cont == 2){
+                    uno.style.display = 'none'
+                    tres.style.display = 'block'
+                }
+            }
+               
+        }else if(e.target == adelante){
+            if(cont < imagenes.length - 1){
+                img.src = imagenes[cont + 1];
+                cont++;
+                console.log(cont)
+                if(cont == 1){
+                    uno.style.display = 'none'
+                    dos.style.display = 'block'
+                }else if(cont == 2){
+                    dos.style.display = 'none'
+                    tres.style.display = 'block'
+                }
+            }else{
+                img.src = imagenes[0];
+                cont = 0;
+                console.log(cont)
+                if(cont == 0){
+                    tres.style.display = 'none'
+                    uno.style.display = 'block'
+                }
+            }
+        }
+    })
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let contenedor = document.querySelector('header');
+    carrousel(contenedor)
+})
+
+
+cerrar.addEventListener('click', () => {
     let menu = document.querySelector('.main ul');
-    if(menu.style.top = '-100px'){
-        menu.style.top = '0px'
-        abrir.style.display = 'none'
+    let body = document.querySelector('body')
+    if(menu.style.top = '0'){
+        menu.style.top = '-100%'
+        abrir.style.display = 'block';
+        cerrar.style.display = 'none'
+        body.classList.remove('fondo')
+        body.style.overflow = 'visible'
+    }
+})
+
+abrir.addEventListener('click', () => {
+    let menu = document.querySelector('.main ul');
+    let body = document.querySelector('body')
+    if(menu.style.top = '-100%'){
+        menu.style.top = '0'
+        abrir.style.display = 'none';
         cerrar.style.display = 'block'
         body.classList.add('fondo')
         body.style.overflow = 'hidden'
     }
-}
-
-function cerrarMenu(){
-    let menu = document.querySelector('.main ul');
-    if(menu.style.top = '0'){
-        menu.style.top = '-100%'
-        cerrar.style.display = 'none'
-        abrir.style.display = 'block'
-        body.classList.remove('fondo')
-        body.style.overflow = 'visible'
-    }
-}
-
-function adelante(a){
-    document.getElementById('header').style.backgroundImage = 'url("images/mobile-image-hero-2.jpg")';
-    let titulo = document.querySelector('.info-hero h2').textContent = 'We are available all across the globe'
-    let texto = document.querySelector('.info-hero p').textContent = 'With stores all over the world, it\'s easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don\'t hesitate to contact us today.'
-}
-
-function atras(){
-    document.getElementById('header').style.backgroundImage = 'url("images/mobile-image-hero-1.jpg")';
-    let titulo = document.querySelector('.info-hero h2').textContent = 'Discover innovative ways to decorate'
-    let texto = document.querySelector('.info-hero p').textContent = 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.'
-}
+})
